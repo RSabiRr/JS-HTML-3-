374240d60d3d44204135fc0391be142ff9119007
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-12-06 22:09:40 +0400

    Add files via upload

---
+++ Array methods, selectors, create element, class list/js/script.js
@@ -0,0 +1,181 @@
+//task 1
+
+// let col=document.querySelector(".row #coll");
+
+// let p=document.createElement("p");
+// let a=document.createElement("a");
+// let button=document.createElement("button");
+
+
+// a.textContent="w3schools";
+// a.href="https://www.w3schools.com/";
+// a.id="w3r";
+// a.type="text/html";
+// a.rel="nofollow";
+// a.target="self";
+
+// button.textContent="Click here to get attributes value";
+
+// p.appendChild(a);
+
+// col.appendChild(p);
+
+// col.appendChild(button);
+// console.log(col);
+
+/////////////////////////////////////////////////
+
+//task 2
+
+// let col2=document.querySelector(".row");
+// let form=document.createElement("form");
+// let p=document.createElement("p");
+// let p2=document.createElement("p");
+// let br=document.createElement("br");
+// let input1=document.createElement("input");
+// let input2=document.createElement("input");
+// let button=document.createElement("button");
+
+// form.id="form1";
+// input1.type="text";
+// input1.name="fname";
+// input1.value="David";
+// input2.type="text";
+// input2.name="lname";
+// input2.value="Beckham";
+
+// button.textContent="Submit";
+
+// p.textContent="First name:";
+// p2.textContent="Last name:";
+
+// p.appendChild(input1);
+// input1.appendChild(br);
+// p2.appendChild(input2);
+// input2.appendChild(br);
+
+
+// form.appendChild(p);
+// form.appendChild(p2);
+
+// form.appendChild(button);
+// col2.appendChild(form);
+
+// console.log(col2);
+
+////////////////////////////
+
+
+// task 3
+
+// let col=document.querySelector(".row #coll");
+
+// let form=document.createElement("form");
+// let select=document.createElement("select");
+
+// let option=document.createElement("option");
+// let option2=document.createElement("option");
+// let option3=document.createElement("option");
+// let option4=document.createElement("option");
+// let button=document.createElement("button");
+
+// form.textContent="Select your favorite Color:";
+
+// select.id="mySelect";
+
+// option.textContent="Red";
+// option2.textContent="Green";
+// option3.textContent="Blue";
+// option4.textContent="White";
+
+// button.textContent="Count and Output all items";
+
+// select.appendChild(option);
+// select.appendChild(option2);
+// select.appendChild(option3);
+// select.appendChild(option4);
+
+// form.appendChild(select);
+
+// col.appendChild(form);
+// col.appendChild(button);
+
+// console.log(col);
+
+///////////////////////////
+
+//task 4
+
+// let col=document.querySelector(".row #coll");
+
+// let table=document.createElement("table");
+
+// let tr=document.createElement("tr");
+// let tr2=document.createElement("tr");
+
+// let td=document.createElement("td");
+// let td2=document.createElement("td");
+// let td3=document.createElement("td");
+// let td4=document.createElement("td");
+
+ 
+// let br=document.createElement("br");
+
+// let button=document.createElement("button");
+
+// table.id="sampleTable";
+// table.setAttribute("border","1");
+
+// td.textContent="Row1 cell1";
+// td2.textContent="Row2 cell2";
+// td3.textContent="Row2 cell2";
+// td4.textContent="Row2 cell2";
+
+  
+// button.textContent="Insert row";
+
+// tr.appendChild(td);
+// tr.appendChild(td2);
+
+// tr2.appendChild(td3);
+// tr2.appendChild(td4);
+
+// table.appendChild(tr);
+// table.appendChild(tr2);
+
+// col.appendChild(table);
+// col.appendChild(br);
+
+// col.appendChild(button);
+
+// console.log(col);
+
+
+///////////////////////////
+
+//task 5
+
+// let roww=document.querySelector(".contanier #roww");
+// let roww2=document.querySelector(".contanier #roww2");
+// let roww3=document.querySelector(".contanier #roww3");
+// let roww4=document.querySelector(".contanier #roww4");
+
+
+// roww.style.width="200px";
+// roww.style.height="150px";
+// roww.style.background="orange"
+// roww.style.border="2px solid green";
+
+// roww2.style.width="200px";
+// roww2.style.height="150px";
+// roww2.style.border="2px solid red";
+
+// roww3.style.width="100px";
+// roww3.style.height="75px";
+// roww3.style.border="2px solid green";
+
+// roww4.style.width="200px";
+// roww4.style.height="150px";
+// roww4.style.border="2px solid red";
+
+///////////////////////////
